f9b0028a7bab279e9e3e0237155a49bcb6b68d52
Nirajan Acharya <nirajanacharya@Nirajans-MacBook-Pro.local>
2023-01-21 12:28:27 -0500

    frontend: Added router and vuex

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,4 +1,17 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/saga-blue/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
 
-createApp(App).mount('#app')
+import router from "./router";
+import store from "./store/index";
+
+const app = createApp(App);
+
+app.use(PrimeVue);
+app.use(router);
+app.use(store);
+
+app.mount("#app");
