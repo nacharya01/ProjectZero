3507473044793433a7de49c1aa860ea77f084f92
Nirajan Acharya <nirajanacharya@Nirajans-MacBook-Pro.local>
2023-01-21 22:21:03 -0500

    client: Adjusted router to only change content

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import { Route, Routes } from "react-router-dom";
-import Home from './pages/Home';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <div className="App">
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
