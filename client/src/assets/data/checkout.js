3dda5ec3a3f99ec964ccd878797f21128132fec5
Nirajan <nacharya01@student.ysu.edu>
2023-03-12 10:07:34 -0400

    checkout_page: Added reserve button, and checkout page for booking

---
+++ client/src/assets/data/checkout.js
@@ -0,0 +1,49 @@
+const checkout = {
+  priceDetails: {
+    title: "Highland Haus- Cozy 70’s A-Frame with Wood Stoves",
+    avgRev: 4.5,
+    totalReview: 50,
+    imageTitle: "Entire chalet",
+    protection: "Your booking is protected by",
+    days: 5,
+    costPerNight: 100,
+    specialOffer: 148,
+    cleaningFee: 70,
+    serviceFee: 88,
+    taxes: 60.67,
+    total: 770.89,
+  },
+  discount: {
+    discountPercentage: "10",
+    daysLeft: "5",
+  },
+  paymentOptions: {
+    bnplText:
+      "Spread the cost of your purchase over time with flexible payment options. Simply select Buy Now, Pay Later at checkout and choose the payment plan that works best for you.",
+    advantagesBnpl: [
+      {
+        title: "Flexible payment options:",
+        detail: "Pay in full or spread the cost over several months.",
+      },
+      {
+        title: "No interest:",
+        detail:
+          "You won't pay any interest as long as you make your payments on time.",
+      },
+    ],
+    advantagesFull: [
+      {
+        title: "Secure payment:",
+        detail: "Your payment information is encrypted and processed securely.",
+      },
+      {
+        title: "No interest:",
+        detail: " You won't pay any interest on your purchase.",
+      },
+    ],
+    fullPayText:
+      "Pay for your purchase in full at checkout. This option is best if you want to avoid paying interest or if you have the funds available to make the purchase right away.",
+  },
+};
+
+export default checkout;
